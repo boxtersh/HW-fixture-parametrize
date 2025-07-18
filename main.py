d390faa17e7310c3b2c62cbda87fffe58bb38017
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-07-18 14:49:20 +0300

    feat (function) test_create_vector,test_len_vector, test_add_vector

---
+++ main.py
@@ -0,0 +1,130 @@
+from random import randint as rint
+import math
+# *************** 1. Vector ****************************************************************
+class Vector:
+
+    def __init__(self, x: int|float, y: int|float, z: int|float)->None:
+        self.x = self.__check_int_float(x)
+        self.y = self.__check_int_float(y)
+        self.z = self.__check_int_float(z)
+
+
+    def __check_int_float(self,number):
+        if not isinstance(number,int|float):
+            raise TypeError('Данные не int или float')
+
+        return number
+
+
+    def get_x(self)->int|float:
+        """
+        Функция возвращает координату X вектора
+        :return: X
+        """
+        return self.x
+
+
+    def get_y(self)->int|float:
+        """
+        Функция возвращает координату Y вектора
+        :return: Y
+        """
+        return self.y
+
+
+    def get_z(self)->int|float:
+        """
+        Функция возвращает координату Z вектора
+        :return: Z
+        """
+        return self.z
+
+
+    def length(self)->int|float:
+        """
+        Функция возвращает длину вектора
+        Пример А(2,1,2) = (2^2 + 1^2 + 2^2)^0.5 = 3
+        :return: int|float
+        """
+        s = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+
+        return round(s,2)
+
+
+    def add(self, other: 'Vector')->'Vector':
+        """
+        Функция возвращает новый вектор
+        как сумму текущего и other вектора
+        Примет А(1,2,3) + В(3,2,1) = АВ(4,4,4)
+        :param other:
+        :return: new vector
+        """
+        x = self.x + other.x
+        y = self.y + other.y
+        z = self.z + other.z
+
+        return Vector(x, y, z)
+
+
+    def sub(self, other: 'Vector') -> 'Vector':
+        """
+        Функция возвращает новый вектор
+        как разность текущего и other вектора
+        Примет А(1,2,3) - В(3,2,1) = АВ(-2,0,2)
+        :param other: переданный вектор
+        :return: новый вектор
+        """
+        x = self.x - other.x
+        y = self.y - other.y
+        z = self.z - other.z
+
+        return Vector(x, y, z)
+
+
+    def scalar_mul(self, other: 'Vector')->int|float:
+        """
+        Функция возвращает скалярное произведение двух векторов
+        текущего и other вектора
+        Пример А(1,2,3) В(3,2,1) = АВ(1*3 + 2*2 + 3*1)
+        :param other: переданный вектор
+        :return: скалярное произведение
+        """
+        scalar = self.x * other.x + self.y * other.y * self.z * other.z
+
+        return round(scalar, 2)
+
+
+    def angle_between(self, other: 'Vector')->int|float:
+        """
+        Функция возвращает угол (в радианах) между текущим и other вектором
+        Пример А(2,1,2) В(1,0,0) =
+        скаляр АВ = (2*1 + 1*0 + 2*0) = 2
+        длина А = (2^2 + 1^2 + 2^2)^0.5 = 3
+        длина В = (1^2 + 0^2 + 0^2)^0.5 = 1
+        cos(угла) = 2 / (3*1)
+        rad = arccos(cos(угла)) = 0.841
+        :param other: переданный вектор
+        :return: radian
+        """
+        scalar = self.scalar_mul(other)
+        length_1 = self.length()
+        length_2 = other.length()
+        cos = scalar / (length_1 * length_2)
+
+        return round(math.acos(cos),2)
+
+
+    @staticmethod
+    def random():
+        x = rint(-100,100)
+        y = rint(-100, 100)
+        z = rint(-100, 100)
+        return Vector(x, y, z)
+
+
+    def __repr__(self):
+        return f'x = {self.x}, y = {self.y}, z = {self.z}'
+
+vec = Vector(-10.3,-5.7,-3.1)
+vec_1 = Vector(1,1,-1)
+print(vec.add(vec_1))
---
+++ test/test_functions_main.py
@@ -0,0 +1,44 @@
+import pytest
+from main import Vector
+
+@pytest.mark.parametrize('x,y,z,expected',
+                         [(1,1,1,True),
+                          (-1,1,1,True),
+                          (1,-1,1,True),
+                          (1,1,-1,True),
+                          (0,0,0,True),
+                          (-10,-5,-3,True),
+                          (-10.3,-5.7,-3.1,True),
+                          (10.3,5.7,3.1,True)])
+
+# @pytest.fixture
+# def vector():
+#     return Vector
+
+def test_create_vector(x,y,z,expected):
+    vec = Vector(x,y,z)
+    expected = True
+    res = isinstance(vec, Vector)
+
+    assert expected == res, f'Ожидали: {expected}, получили: {res}'
+
+
+def test_len_vector():
+    vec = Vector(-10.3,-5.7,-3.1)
+    expected = 12.17
+    res = vec.length()
+
+    assert expected == res, f'Ожидали: {expected}, получили: {res}'
+
+def test_add_vector():
+    vec = Vector(-10.3, -5.7, -3.1)
+    vec_1 = Vector(1,1,-1)
+    expected = -9.3
+    new_vec = vec.add(vec_1)
+    assert expected == new_vec.get_x(), f'Ожидали: {expected}, получили: {new_vec.get_x()}'
+    expected = -4.7, -4.1
+    assert expected == new_vec.get_y(), f'Ожидали: {expected}, получили: {new_vec.get_y()}'
+    expected = -4.1
+    assert expected == new_vec.get_z(), f'Ожидали: {expected}, получили: {new_vec.get_z()}'
+
+
